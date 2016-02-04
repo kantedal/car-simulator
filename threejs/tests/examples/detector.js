092e14edbd5cecac2beaa0d7bc1a9d3d4d2fac40
kantedal <kantedal@gmail.com>
2016-02-05 00:02:51 +0100

    a

--- threejs/tests/examples/detector.js
+++
@@ -1,9 +0,0 @@
-/// <reference path="../../three.d.ts" />
-/// <reference path="../three-tests-setup.ts" />
-(function () {
-    if (!Detector.canvas || !Detector.webgl || !Detector.workers || !Detector.fileapi) {
-        var errorElement = Detector.getWebGLErrorMessage();
-        Detector.addGetWebGLMessage();
-    }
-});
-//# sourceMappingURL=detector.js.map
